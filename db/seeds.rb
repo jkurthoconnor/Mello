1c9023102ab452f4e3c3c760cd258935345d8eae
Alex Soloviev <alex.soloviev@gmail.com>
2019-06-25 15:44:58 -0400

    fixed seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Board.destroy_all
+Card.destroy_all
+List.destroy_all
+
 board1 = Board.create(title: "NEW Items")
 Board.create(title: "More things")
 Board.create(title: "Project todos")
 Board.create(title: "Another board")
 
 list1 = List.create(title: "New List", board: board1)
+list2 = List.create(title: "Another List", board: board1)
 
-card = Card.create(title: "card_1", list: list1, labels: ['blue', 'yellow'], due_date: 2.days.from_now, comments_count: 25)
+card1 = Card.create(title: "card_1", list: list1, labels: ['blue', 'yellow'], due_date: 2.days.from_now, comments_count: 25)
+card2 = Card.create(title: "card_2", list: list1, labels: ['blue', 'red'], due_date: 2.days.from_now, comments_count: 2)
+
+card3 = Card.create(title: "card_3", list: list2, labels: ['blue'], due_date: 2.days.from_now, comments_count: 5)
+card4 = Card.create(title: "card_4", list: list2, labels: ['brown'], due_date: 2.days.from_now, comments_count: 25)
+card5 = Card.create(title: "card_5", list: list2, labels: ['blue', 'yellow'], due_date: 2.days.from_now, comments_count: 25)
