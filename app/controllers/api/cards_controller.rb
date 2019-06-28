618399cbdcb361ca23787b6cd1dd12150ba66953
Alex Soloviev <alex.soloviev@gmail.com>
2019-06-28 14:10:22 -0400

    created show action on cards controller
    Co-authored-by: Faazil Shaikh <faazil.shaikh@gmail.com>
    Co-authored-by: Kurth O'Connor <jkurthoconnor@gmail.com>
    Co-authored-by: Alex Soloviev <alex.soloviev@gmail.com>

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,6 +1,10 @@
 class Api::CardsController < ApplicationController
   def show
+    @card = Card.find(params[:id])
 
+    rescue ActiveRecord::RecordNotFound
+    @error = "Invalid card id"
+    render 'api/shared/error', status: :not_found    
   end
 
   def create
