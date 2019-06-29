c1580f36f603b41b81a2b1eb9bbda7d0983e4c17
jkurthoconnor <jkurthoconnor@gmail.com>
2019-06-29 10:57:02 -0600

    Remove unused state from Application.jsx
    Co-authored-by: Faazil Shaikh <faazil.shaikh@gmail.com>
    Co-authored-by: Kurth O'Connor <jkurthoconnor@gmail.com>
    Co-authored-by: Alex Soloviev <alex.soloviev@gmail.com>

--- app/javascript/components/Application.jsx
+++ app/javascript/components/Application.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 import TopNav from './shared/TopNav';
 import BoardsDashboardContainer from './dashboard/BoardsDashboardContainer';
 import BoardContainer from './dashboard/BoardContainer';
 
 class Application extends React.Component {
-  static contextTypes = {
-    store: PropTypes.object
-  }
-
-  componentDidMount() {
-    const store = this.context.store;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
   render() {
-    const state = this.context.store.getState();
-
     return (
       <div>
         <TopNav />
