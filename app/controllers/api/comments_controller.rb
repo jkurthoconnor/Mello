570fae2f454bc2f37ae7febb162f8d2c70e56f8b
4g3m <13265214+z3r033@users.noreply.github.com>
2019-07-02 12:11:14 -0700

    comment model + controller is working
    Co-authored-by: Kurth O'Connor <jkurthoconnor@gmail.com>
    Co-authored-by: Alex Soloviev <alex.soloviev@gmail.com>

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::CommentsController < ApplicationController
+  def create
+    @card = Card.find(params[:card_id])
+    @comment = Comment.new(comment_params.merge({card: @card}))
+
+    if @comment.save
+      render :create, status: :created
+    else
+      @error = @comment.errors.full_messages.join(", ")
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+
+  rescue ActionController::ParameterMissing
+    @error = "Invalid comment data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
